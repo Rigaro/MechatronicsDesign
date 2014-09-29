7b8c41235c6de226f6fa3d3bcf093ba0a74cf343
Prithu Parker <prithu@iinet.net.au>
2014-09-30 00:09:48 +1000

    Cleaned up and fixed PID control. Signal clamping is now done in a single method, integral and derivatives are now calculated correctly.

--- controllerpid.cpp
+++ controllerpid.cpp
@@ -4,80 +4,86 @@ ControllerPID::ControllerPID() : Controller()
 {
     gainI = 0;
     gainD = 0;
-    samplingFreq = 100;
+    samplingFreq = 10;
     integral = 0;
     derivative = 0;
+    previousError = 0;
 }
 
-ControllerPID::ControllerPID(int outputMin_deg, int outputMax_deg, double gainP,
-                             double gainI, double gainD, double samplingFreq)
+ControllerPID::ControllerPID(int outputMin_deg, int outputMax_deg, 
+                             double gainP, double gainI, double gainD)
                             : Controller(outputMin_deg, outputMax_deg, gainP)
 {
     this->gainI = gainI;
     this->gainD = gainD;
-    if(samplingFreq > 0)
-        this->samplingFreq = samplingFreq;
+
+    samplingFreq = 10;
     integral = 0;
     derivative = 0;
 }
 
-//Performs the integral correction for the current position.
-//If the correction is greater or lower than the controller's limits,
-//the value is set to that of the limit.
-//@return controller proportional correction as integer.
-double ControllerPID::IntegralCorrection()
+void ControllerPID::ComputeError()
 {
-    integral += error_k0/samplingFreq;
-    double correctionI = gainI * integral;
-    if(correctionI > UPPERLIMIT)
-        correctionI = UPPERLIMIT;
-    else if(correctionI < LOWERLIMIT)
-        correctionI = LOWERLIMIT;
-    return correctionI;
+    previousError = error;
+    error = this->desPos_px - this->curPos_px;
 }
 
-//Performs the derivative correction for the current position.
-//If the correction is greater or lower than the controller's limits,
-//the value is set to that of the limit.
-//@return controller proportional correction as integer.
-double ControllerPID::DerivativeCorrection()
+/**
+Performs the integral correction for the current position.
+@return controller proportional correction as integer.
+*/
+double ControllerPID::IntegralCorrection(double delta)
 {
-    derivative = (error_k0 - error_k1)*samplingFreq;
-    double correctionD = gainD * derivative;
-    if(correctionD > UPPERLIMIT)
-        correctionD = UPPERLIMIT;
-    else if(correctionD < LOWERLIMIT)
-        correctionD = LOWERLIMIT;
-    return correctionD;
+    integral = integral + gainI*error*delta;
+
+    return integral;
 }
 
-//Performs PID position control that sets the desired position.
-//The output is normalized to work within the application's requirements.
-//@return normalized control signal.
-int ControllerPID::PositionControl(int desPos_px, int curPos_px)
+/**
+Performs the derivative correction for the current position.
+@return controller proportional correction as integer.
+*/
+double ControllerPID::DerivativeCorrection(double delta)
 {
-    SetCurrentPos_px(curPos_px);
+    derivative = gainD * (error - previousError) / delta;
+
+    return derivative;
+}
+
+/**
+Performs PID position control, uses sampling frequency for frame delta
+calculation
+*/
+int ControllerPID::PositionControl(int desPox_px, int curPos_px)
+{
+    double frameDelta = 1 / samplingFreq;
+    return PositionControl(desPos_px, curPos_px);
+}
+
+/**
+Performs PID position control that sets the desired position.
+The output is normalized to work within the application's requirements.
+@return normalized control signal.
+*/
+int ControllerPID::PositionControl(int desPos_px, int curPos_px, double frameDelta)
+{
     SetDesiredPos_px(desPos_px);
-    ComputeError();
-    controlSignal = ProportionalCorrection() + IntegralCorrection() + DerivativeCorrection();
-    if(controlSignal > UPPERLIMIT)
-        controlSignal = UPPERLIMIT;
-    else if(controlSignal < LOWERLIMIT)
-        controlSignal = LOWERLIMIT;
-    return NormalizeData(controlSignal);
+    
+    return PositionControl(curPos_px, frameDelta);
 }
 
-//Performs PID position control.
-//The output is normalized to work within the application's requirements.
-//@return normalized control signal.
-int ControllerPID::PositionControl(int curPos_px)
+/**
+Performs PID position control.
+The output is normalized to work within the application's requirements.
+@return normalized control signal.
+*/
+int ControllerPID::PositionControl(int curPos_px, double frameDelta)
 {
     SetCurrentPos_px(curPos_px);
     ComputeError();
+
     controlSignal = ProportionalCorrection() + IntegralCorrection() + DerivativeCorrection();
-    if(controlSignal > UPPERLIMIT)
-        controlSignal = UPPERLIMIT;
-    else if(controlSignal < LOWERLIMIT)
-        controlSignal = LOWERLIMIT;
+    controlSignal = ClampSaturation(controlSignal);
+
     return NormalizeData(controlSignal);
 }
