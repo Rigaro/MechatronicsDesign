8c751ae77403e5bce22fde8e2f55d5dd769a3c01
Prithu Parker <prithu@iinet.net.au>
2014-10-01 18:48:48 +1000

    Renamed controllertilt to tiltcontroller

---
+++ tiltcontroller.cpp
@@ -0,0 +1,12 @@
+#include "tiltcontroller.h"
+
+ControllerTilt::ControllerTilt() : Controller()
+{
+
+}
+
+ControllerTIlt::ControllerTilt(int desPox_px, int curPos_px,
+                               double delay) : ControllerTilt()
+{
+
+}
